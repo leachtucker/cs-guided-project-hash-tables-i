d83764177f279e749f86687def90604fd6f1a7d0
Tucker <leachtucker@gmail.com>
2021-03-23 20:23:06 -0700

    Solved demo2

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -24,29 +24,56 @@ hash_table.remove("b");         // remove the mapping for 2
 hash_table.get("b");            // returns -1 (not found)
 ```
 """
-class ListNode:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
-
 class MyHashTable:
-    def __init__(self):
-        # Your code here
+    def __init__(self, capacity):
+        self.storage = [None] * capacity
+        self.capacity = capacity
+        self.occupied = 0
 
-       
-    # Your code here
+    def hash(self, key):
+        # DJB2 HASH
+        str_key = str(key).encode()
 
-    
-    
+        hash_value = 5381
+
+        # Bit-shift
+        for b in str_key:
+            hash_value = ((hash_value << 5) + hash_value) + b
+            hash_value &= 0xffffffff
+
+        return hash_value
+
+    def hash_index(self, key):
+        # Return an integer index within the boundaries of the underlying storage (list)
+        return self.hash(key) % self.capacity
+
     def put(self, key, value):
-        # Your code here
+        index = self.hash_index(key)
+        self.storage[index] = value
+        return index
 
 
     def get(self, key):
-        # Your code here
+        index = self.hash_index(key)
+        return self.storage[index]
 
 
-    def remove(self, key: int) -> None:
-        # Your code here
+    def remove(self, key):
+        index = self.hash_index(key)
+        self.storage[index] = None
 
+        return self.storage[index]
+
+
+myTable = MyHashTable(36)
+myTable.put('apple', 'fruit')
+myTable.put('cucumber', 'vegetable')
+myTable.put('broccoli', 'vegetable')
+
+print(myTable.get('apple'))
+print(myTable.get('cucumber'))
+print(myTable.get('broccoli'))
+print('\n')
+
+myTable.remove('apple')
+print(myTable.get('apple'))
